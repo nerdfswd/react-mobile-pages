e46abe0384365fb9b4bbfd0fe84748b357e22721
sabiyatabassum <sabiyatabassum99@gmail.com>
2020-08-03 20:06:17 +0530

    Updates

---
+++ precache-manifest.f2252abf51beb7505e2547213c2be38e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af13d04deee0699e9d65f28701e5ee8",
+    "url": "/react-mobile-pages/index.html"
+  },
+  {
+    "revision": "225e8ada1c7169f520aa",
+    "url": "/react-mobile-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7ed2a041b08d1b32f8e5",
+    "url": "/react-mobile-pages/static/js/2.a814797f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-mobile-pages/static/js/2.a814797f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225e8ada1c7169f520aa",
+    "url": "/react-mobile-pages/static/js/main.35d002c7.chunk.js"
+  },
+  {
+    "revision": "79a1256f8630bf65637b",
+    "url": "/react-mobile-pages/static/js/runtime-main.1febac81.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-mobile-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
